7ef52f423f6d4888645ff801c525ad25d11ce348
Anchal Singh <Dell-14@Anchal-Singh.GEMSRV.COM>
2022-06-04 21:28:25 +0530

    anamika updated leadership pages

--- element/leader.js
+++ element/leader.js
@@ -31,24 +31,20 @@ console.log(sliderRef.current);
                className="leader_tht" >
                   <div>
                   <img src="/images/background/leader1.jpg" alt="" />
-                 <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+                 <p className={Styles.leaderpara}><a href="/leadership-1"  target="_blank"><span>WHY INDIAN SMEs MARKETING IS LENDING BEHIND IN SALES?</span><i></i></a></p>
                   </div>
                  
                   <div>
                   <img src="/images/background/leader2.jpg" alt="" />
-                  <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+                  <p className={Styles.leaderpara}><a href="/leadership-2"  target="_blank"><span>Indian SMEs: What should they learn from our Leadership Thoughts?</span><i></i></a></p>
                   </div>
                   <div>
                   <img src="/images/background/leader3.jpg" alt="" />
-                  <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+                  <p className={Styles.leaderpara}><a href="/leadership-2"  target="_blank"><span>Here are some key pointers for your marketing technique:-</span><i></i></a></p>
                   </div>
                   <div>
                   <img src="/images/background/leader4.jpg" alt="" />
-                  <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+                  <p className={Styles.leaderpara}><a href="/leadership-1"  target="_blank"><span>WHY INDIAN SMEs MARKETING IS LENDING BEHIND IN SALES?</span><i></i></a></p>
                   </div>
                
                 </Slider>
@@ -65,23 +61,19 @@ our clients, and partners.</p>
       
           <div>
           <img src="/images/background/leader2.jpg" alt="" />
-          <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+          <p className={Styles.leaderpara}><a href="/leadership-1"  target="_blank"><span>WHY INDIAN SMEs MARKETING IS LENDING BEHIND IN SALES?</span><i></i></a></p>
           </div>
           <div>
           <img src="/images/background/leader3.jpg" alt="" />
-          <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+          <p className={Styles.leaderpara}><a href="/leadership-2"  target="_blank"><span>Indian SMEs: What should they learn from our Leadership Thoughts?</span><i></i></a></p>
           </div>
           <div>
           <img src="/images/background/leader4.jpg" alt="" />
-          <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+          <p className={Styles.leaderpara}><a href="/leadership-2"  target="_blank"><span>Here are some key pointers for your marketing technique:-</span><i></i></a></p>
           </div>
           <div>
           <img src="/images/background/leader1.jpg" alt="" />
-          <p className={Styles.leaderpara}>Our expertise teams are creating breakthroughs for
-our clients, and partners.</p>
+          <p className={Styles.leaderpara}><a href="/leadership-1"  target="_blank"><span>WHY INDIAN SMEs MARKETING IS LENDING BEHIND IN SALES?</span><i></i></a></p>
           </div>
        
         </Slider>
